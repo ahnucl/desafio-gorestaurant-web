40be0174ccf848ca4453ab82e59c0a28bc5362ff
Leonardo Cunha <lalmcunha@gmail.com>
2020-09-22 21:08:46 -0300

    Os testes estão passando, mas o availability não é alterado no backend

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -86,7 +86,6 @@ const Dashboard: React.FC = () => {
   }
 
   function handleEditFood(food: IFoodPlate): void {
-    // TODO SET THE CURRENT EDITING FOOD ID IN THE STATE
     setEditingFood(food);
     toggleEditModal();
   }
